c3a9c3bf1bda6c1960f1a716ca97189ef659304f
Shubham <sk4957199@gmail.com>
2023-05-16 10:18:46 +0530

    added new api

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request,make_response
 from flask_jwt_extended import create_access_token
+# from flask_jwt_extended import jwt_required,get_raw_jwt_token, get_jwt_identity
+from flask_jwt_extended import jwt_required, unset_jwt_cookies,  get_jwt_identity
 from app.models.user import User
 
 auth_blueprint = Blueprint('auth', __name__)
@@ -44,8 +46,90 @@ def login():
     return jsonify({'message': 'Login successful', 'access_token': access_token}), 200
 
 
+
+
+@auth_blueprint.route('/profile', methods=['PUT'])
+@jwt_required()
+def update_profile():
+    current_user = get_jwt_identity()
+    user = User.find_by_email(email=current_user)
+
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email') 
+
+       # Update the user's name if provided
+    if name:
+        user.name = name
+
+    # Update the user's email if provided and not already taken
+    if email and email != user.email:
+        if User.query.filter_by(email=email).first():
+            return jsonify({'message': 'Email already exists'}), 400
+        user.email = email
+
+    # Save the updated user
+    user.save()
+
+    return jsonify({'message': 'Profile updated successfully'}), 200  
+
+@auth_blueprint.route('/me', methods=['GET'])
+@jwt_required()
+def get_profile():
+    current_user = get_jwt_identity()
+    user = User.find_by_email(current_user)
+
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    profile = {
+        'name': user['name'],
+        'email': user['email'],
+        # 'skills': user['skills'],
+        # 'bio': user['bio']
+    }
+
+    return jsonify(profile), 200
 # logout
 
+
+@auth_blueprint.route('/logout', methods=['POST'])
+@jwt_required()
+def logout():
+    user = User.find_by_email(get_jwt_identity())
+
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    # Clear the access token cookie
+    response = make_response(jsonify({'message': 'Logout successful'}), 200)
+    unset_jwt_cookies(response)
+
+    return response
+
+
+
+
+
+@auth_blueprint.route('/delete', methods=['DELETE'])
+@jwt_required()
+def delete_user():
+    current_user = get_jwt_identity()
+    user = User.find_by_email(current_user)
+
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+     # Delete the user
+    user_instance = User(user['name'], user['email'], user['password'])
+    user_instance.delete()
+
+    return jsonify({'message': 'User deleted successfully'}), 200
+
+# Regenerate response
 # get profile
 
 # update profile
